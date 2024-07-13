14b8a93aa9f8bbeab3c20f0af92978cbaa59226f
AbdElhalimA3 <aabdelh6@corp.vodafone.es>
2024-07-13 23:39:15 +0300

    add files

---
+++ python.py
@@ -0,0 +1,13 @@
+import requests
+
+webhook_url = "https://vodafone.webhook.office.com/webhookb2/eb0c06d1-590f-4bac-b21d-fd6a41e86cde@68283f3b-8487-4c86-adb3-a5228f18b893/IncomingWebhook/471c53e15e7d4ddb9fc6fdc5eb0c6001/8073a3a4-4a6a-4909-bacf-f7eff6c2fcc4"
+message = {
+    "text": "http://{jenkins_url}/job/{job_name}/{build_number}/"
+}
+response = requests.post(webhook_url, json=message)
+if response.status_code == 200:
+    print("Message sent successfully!")
+else:
+    print(f"Failed to send message. Status code: {response.status_code}")
+
+http://{jenkins_url}/job/{job_name}/{build_number}/
